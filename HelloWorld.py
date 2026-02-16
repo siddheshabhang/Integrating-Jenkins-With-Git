2ef1b420e917f24885081118a9b3be37ea150285
Siddhesh Abhang <1908siddhesh@gmail.com>
2026-02-16 14:58:24 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,8 +1,3 @@
 print("Hello, I am Learning Jenkins GitHub Hook Trigger for GitSCM polling!")
-print("Hello, I am Learning Jenkins GitHub Hook Trigger for GitSCM polling!")
-print("Hello, I am Learning Jenkins GitHub Hook Trigger for GitSCM polling!")
-print("Hello, I am Learning Jenkins GitHub Hook Trigger for GitSCM polling!")
-print("Hello, I am Learning Jenkins GitHub Hook Trigger for GitSCM polling!")
-print("Hello, I am Learning Jenkins GitHub Hook Trigger for GitSCM polling!")
-print("Hello, I am Learning Jenkins GitHub Hook Trigger for GitSCM polling!")
+print("Hello, I am L
 
